dddd03fd791ca35f97add59d094897b3cbd02d6a
Andrew Janke <floss@apjanke.net>
2019-03-02 00:40:37 -0500

    Add pp() alias

---
+++ inst/pp.m
@@ -0,0 +1,9 @@
+function pp(x)
+  %PP Alias for prettyprint, for debugging
+  %
+  % pp(x)
+  %
+  % This is just an alias for prettyprint(). It is provided as a debugging convenience,
+  % and will be removed before a 1.0 release of `table`.
+  prettyprint(x);
+endfunction
