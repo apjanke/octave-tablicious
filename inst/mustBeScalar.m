367b5757ed7e5677ee6d2642168dd9c7d11a4271
Andrew Janke <andrew@apjanke.net>
2019-05-02 19:50:30 -0400

    Add texinfo for mustBeScalar

--- inst/mustBeScalar.m
+++ inst/mustBeScalar.m
@@ -13,6 +13,20 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{x} =} mustBeScalar (@var{x}, @var{label})
+##
+## Requires that input is scalar.
+##
+## Raises an error if the input @var{x} is not scalar, as determined by
+## @code{isscalar (x)}.
+##
+## @var{label} is an optional input that determines how the input will be described in
+## error messages. If not supplied, @code{inputname (1)} is used, and if that is
+## empty, it falls back to "input".
+##
+## @end deftypefn
+
 function mustBeScalar (x)
   if ~isscalar (x)
     name = inputname (1);
