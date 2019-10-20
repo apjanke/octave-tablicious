ad552ded417bf024ddb994763e43c5f3ae6dcdc6
Andrew Janke <andrew@apjanke.net>
2019-10-19 17:37:37 -0700

    WIP: table I/O: A bit of readtable action (incomplete)
    This is an initial attempt at readtable and some related Table I/O stuff. In fact, it's two attempts that I made a few months apart, combined together to make them easy to compare and merge.
    
    Nothing in here is really working yet.

---
+++ inst/detectImportOptions.m
@@ -0,0 +1,22 @@
+## Copyright (C) 2020 Andrew Janke
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function} {@var{out} =} detectImportOptions (@var{filename})
+## @deftypefnx {Function} {@var{out} =} detectImportOptions (@dots{}, @var{Name}, @var{Value})
+##
+## Detect import options based on file content.
+##
+## @end deftypefn
