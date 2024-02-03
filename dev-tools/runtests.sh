a8a7087bdd2bb634d9cab3e600b0e0e77ce70432
Andrew Janke <andrew@apjanke.net>
2024-02-03 17:51:37 -0500

    [test] refine the runtests.sh script for output capture and running from any directory
    This makes it diverge from the "standard" runtests.sh script that I think is found in other Octave Forge packages. I'm fine with that; this script can be Tablicious-specific. (It's in the nonstandard dev-tools/ subdir, so I don't think any standard Octave tools like pkg could be depending on it.)

--- dev-tools/runtests.sh
+++ dev-tools/runtests.sh
@@ -2,43 +2,74 @@
 #
 # Test wrapper for Octave tests.
 #
+# BROKEN: As of Octave 8.x (and maybe earlier), it looks like runtests() no longer
+# exists, and this script will error out with a "'runtests' undefined..." error.
+#
 # This exists because Octave's runtests() does not return an error status, so
 # you can't detect within octave whether your tests have failed.
 #
-# This must be run from the root of the repo.
-#
-# Prerequisite: "make local"
+# Prerequisite: "make local" must have been run on this packaage
 
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
-package=$(grep "^Name: " DESCRIPTION | cut -f2 -d' ')
+# Utility functions
 
-#OCTAVE="octave --no-gui --norc"
-# --no-gui might be causing build failures on Linux???
+THIS_PROGRAM=$(basename $0)
+
+function info() {
+  echo "$*"
+}
+
+function error() {
+  echo >&2 "${THIS_PROGRAM}: ERROR: $*"
+}
+
+# Main script code
+
+pkg_dir=$(dirname $(realpath $(dirname "$0")))
+inst_dir="${pkg_dir}/inst"
+test_dir="$inst_dir"
+package='tablicious'
 OCTAVE="octave --norc --no-gui"
+TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
 
-test_dir="$1"
+outfile="octave-${package}-tests-${TIMESTAMP}.log"
+oct_test_code="addpath('$test_dir'); runtests('$test_dir')"
 
-tempfile=$(mktemp /tmp/octave-${package}-tests-XXXXXXXX)
-if [[ "$test_dir" == "" ]]; then
-  ${OCTAVE} --path="$PWD/inst" --eval="runtests" 2>&1 | tee "$tempfile"
-else
-  ${OCTAVE} --path="$PWD/inst" --eval="addpath('$test_dir'); runtests('$test_dir')" 2>&1 | tee "$tempfile"
-fi
+# Main test run
 
-if grep FAIL "$tempfile" &>/dev/null; then
-  echo runtests.sh: Some tests FAILED!
+info "Running tests for package ${package}"
+
+${OCTAVE} --path="$inst_dir" --eval="${oct_test_code}" 2>&1 | tee "$outfile"
+
+if grep FAIL "$outfile" &>/dev/null; then
   status=1
+  info "Some tests FAILED!"
 else
-  echo runtests.sh: All tests passed.
   status=0
+  info "All tests passed."
+fi
+echo ''
+
+info "Running additional tests so we can get output"
+things_to_test=( datetime duration string table )
+for thing in "${things_to_test[@]}"; do
+  info "Testing ${thing}"
+  ${OCTAVE} --path="$PWD/inst" --eval="addpath('$test_dir'); test ${thing}" 2>&1 | tee -a "$outfile"
+done
+
+if [[ $status = 0 ]]; then
+  extra_fail=''
+else
+  extra_fail='SOME TESTS FAILED!'
 fi
 
-echo "runtests.sh: running additional tests so we can get output"
-echo "runtests.sh: testing datetime"
-${OCTAVE} --path="$PWD/inst" --eval="addpath('$test_dir'); test datetime" 2>&1 | tee "$tempfile"
-echo "runtests.sh: testing duration"
-${OCTAVE} --path="$PWD/inst" --eval="addpath('$test_dir'); test duration" 2>&1 | tee "$tempfile"
+echo ''
+info "Done running tests. Exiting with status ${status}. ${extra_fail}"
+info "Test output is in file: ${outfile}"
+echo ''
 
-rm "$tempfile"
-exit $status
+exit "$status"
