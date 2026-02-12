dc0ebbbf7b5ed2432ff8a0c49d519e8a6c7fbee1
Andrew Janke <andrew@apjanke.net>
2026-02-12 11:54:34 -0500

    [doc] update copyright years to add 2025 and 2026 in source headers

--- inst/+tblish/+internal/+datasets/@attitude/attitude.m
+++ inst/+tblish/+internal/+datasets/@attitude/attitude.m
@@ -1,5 +1,5 @@
 ## Copyright (C) 1995-2013 R Core Team
-## Copyright (C) 2019, 2023, 2024 Andrew Janke
+## Copyright (C) 2019, 2023, 2024, 2026 Andrew Janke
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
